4ef2e33037728fdef6f6dfcba8f5312c7e5c141f
James Leitch <me@rickvanprim.com>
2024-07-21 11:50:46 -0700

    RC5 Cipher

--- crates/longboy/src/proto/cipher.rs
+++ crates/longboy/src/proto/cipher.rs
@@ -1,25 +1,45 @@
-pub(crate) struct Cipher<const SIZE: usize> {}
+use cipher::{
+    array::Array,
+    typenum::{U20, U4, U8},
+    BlockCipherDecrypt, BlockCipherEncrypt,
+};
+use rc5::RC5;
+
+pub(crate) struct Cipher<const SIZE: usize>
+{
+    header_cipher: RC5<u16, U20, U8>,
+    slot_cipher: RC5<u32, U20, U8>,
+}
 
 impl<const SIZE: usize> Cipher<SIZE>
 {
     pub(crate) fn new(key: u64) -> Self
     {
-        Self {}
+        Self {
+            header_cipher: RC5::new(key.to_ne_bytes().as_ref()),
+            slot_cipher: RC5::new(key.to_ne_bytes().as_ref()),
+        }
     }
 
     pub(crate) fn encrypt_header(&self, block: &mut [u8; 4])
     {
+        self.header_cipher.encrypt_block(block.as_mut())
     }
 
     pub(crate) fn decrypt_header(&self, block: &mut [u8; 4])
     {
+        self.header_cipher.decrypt_block(block.as_mut())
     }
 
     pub(crate) fn encrypt_slot(&self, block: &mut [u8; SIZE])
     {
+        self.slot_cipher
+            .encrypt_blocks(Array::cast_slice_from_core_mut(block.as_chunks_mut().0))
     }
 
     pub(crate) fn decrypt_slot(&self, block: &mut [u8; SIZE])
     {
+        self.slot_cipher
+            .decrypt_blocks(Array::cast_slice_from_core_mut(block.as_chunks_mut().0))
     }
 }
